f08c892a03a3d44fec25a1a13c8a5b60b97c5593
son426 <coghks0426@naver.com>
2023-05-29 17:16:10 +0900

    docker

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL = `http://cluver.kr:8000`;
+// export const BASE_URL = `http://cluver.kr:8000`;
+export const BASE_URL = `http://localhost:8000`;
 
 export async function loginValidate(id: string, password: string) {
   try {
