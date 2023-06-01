a9191dde934a0db43f2dae3d9e1aab08f3e5fc69
son426 <coghks0426@naver.com>
2023-06-01 19:39:30 +0900

    토큰 쿠키 체크

--- src/pages/test/test.tsx
+++ src/pages/test/test.tsx
@@ -3,9 +3,9 @@ import axios from "axios";
 // api 테스트용 페이지
 // 허락없이 추가한 것에 심심한 양해의 말씀을..
 function Test() {
-  const test_api = async () => {
-    const SERVER_URL = "cluver.kr:8000";
+  const SERVER_URL = "cluver.kr:8000";
 
+  const login = async () => {
     const response = await axios({
       method: "post",
       url: `http://${SERVER_URL}/auth/test`,
@@ -14,9 +14,19 @@ function Test() {
     console.log(response);
   };
 
+  const authorization = async () => {
+    const response = await axios({
+      method: "get",
+      url: `http://${SERVER_URL}/auth/check`,
+      withCredentials: true,
+    });
+    console.log(response);
+  };
+
   return (
     <>
-      <button onClick={test_api}>api 테스트</button>
+      <button onClick={login}>accessToken 발급</button>
+      <button onClick={authorization}>인증 체크</button>
     </>
   );
 }
